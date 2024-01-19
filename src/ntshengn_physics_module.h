658d6c46de951c30b7c2bf2ca6ff363ea23c4862
ntsh-oni <teamnutshell.dev@gmail.com>
2024-01-19 22:36:08 +0100

    remove gjk and epa functions

--- src/ntshengn_physics_module.h
+++ src/ntshengn_physics_module.h
@@ -45,46 +45,6 @@ struct NarrowphaseCollision {
 	std::vector<NtshEngn::Math::vec3> intersectionPoints;
 };
 
-class GJKSimplex {
-public:
-	GJKSimplex& operator=(std::initializer_list<NtshEngn::Math::vec3> list) {
-		for (std::initializer_list<NtshEngn::Math::vec3>::iterator it = list.begin(); it != list.end(); it++) {
-			m_points[std::distance(list.begin(), it)] = *it;
-		}
-		m_size = list.size();
-
-		return *this;
-	}
-
-	void push_front(const NtshEngn::Math::vec3& point) {
-		m_points = { point, m_points[0], m_points[1], m_points[2] };
-		m_size = std::min(m_size + 1, static_cast<size_t>(4));
-	}
-
-	NtshEngn::Math::vec3& operator[](size_t i) {
-		return m_points[i];
-	}
-
-	size_t size() {
-		return m_size;
-	}
-
-	std::array<NtshEngn::Math::vec3, 4>::iterator begin() {
-		return m_points.begin();
-	}
-
-	std::array<NtshEngn::Math::vec3, 4>::iterator end() {
-		return m_points.end() - (4 - m_size);
-	}
-
-private:
-	std::array<NtshEngn::Math::vec3, 4> m_points = { NtshEngn::Math::vec3(0.0f, 0.0f, 0.0f),
-		NtshEngn::Math::vec3(0.0f, 0.0f, 0.0f),
-		NtshEngn::Math::vec3(0.0f, 0.0f, 0.0f),
-		NtshEngn::Math::vec3(0.0f, 0.0f, 0.0f) };
-	size_t m_size = 0;
-};
-
 namespace NtshEngn {
 
 	class PhysicsModule : public PhysicsModuleInterface {
@@ -126,16 +86,6 @@ namespace NtshEngn {
 		IntersectionInformation intersect(const ColliderCapsule* capsule, const ColliderBox* box);
 		IntersectionInformation intersect(const ColliderCapsule* capsule, const ColliderSphere* sphere);
 
-		IntersectionInformation gjk(const ColliderShape* shape1, const ColliderShape* shape2);
-
-		bool simplexContainsOrigin(GJKSimplex& simplex, NtshEngn::Math::vec3& direction);
-
-		bool gjkLine(GJKSimplex& simplex, Math::vec3& direction);
-		bool gjkTriangle(GJKSimplex& simplex, Math::vec3& direction);
-		bool gjkTetrahedron(GJKSimplex& simplex, Math::vec3& direction);
-
-		bool sameDirection(const Math::vec3& a, const Math::vec3& b);
-
 		Math::vec3 getCenter(const ColliderShape* shape);
 		Math::vec3 getCenter(const ColliderSphere* sphere);
 		Math::vec3 getCenter(const ColliderBox* box);
@@ -147,8 +97,6 @@ namespace NtshEngn {
 		Math::vec3 getFarthestPointInDirection(const ColliderSphere* sphere, const Math::vec3& direction);
 		Math::vec3 getFarthestPointInDirection(const ColliderBox* box, const Math::vec3& direction);
 		Math::vec3 getFarthestPointInDirection(const ColliderCapsule* capsule, const Math::vec3& direction);
-
-		std::tuple<Math::vec3, float, std::pair<Math::vec3, Math::vec3>> epa(const ColliderShape* shape1, const ColliderShape* shape2, GJKSimplex& simplex);
 
 		std::pair<std::vector<Math::vec4>, size_t> getPolytopeNormals(const std::vector<Math::vec3>& polytope, const std::vector<size_t>& faces);
 		void addIfUniqueEdge(std::vector<std::pair<size_t, size_t>>& edges, const std::vector<size_t>& polytopeIndices, size_t a, size_t b);
